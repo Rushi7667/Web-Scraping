3cc189cefada3a7b9d55ed72866fc92d9dff1a8d
Rushi <rushilunagariya7667@gmail.com>
2023-05-20 07:24:55 +0530

    added new code for multiple page scraping

--- whiskyscraper/whiskyscraper/spiders/WhiskeySpider.py
+++ whiskyscraper/whiskyscraper/spiders/WhiskeySpider.py
@@ -4,18 +4,23 @@ from scrapy.spiders import Spider
 class WhiskeySpider(scrapy.Spider):
     name = 'whisky'
     start_urls = ['https://www.whiskyshop.com/scotch-whisky?item_availability=In+Stock&product_list_limit=96']
-
+                
     def parse(self, response):
         for products in response.css('div.product-item-info'):
             try:
                 yield {
                     'name' :  products.css('a.product-item-link::text').get(),
-                    'price' : products.css('span.price::text').get().replace(' £',''),
+                    'price in £' : products.css('span.price::text').get().replace('£',''),
                     'link' : products.css('a.product-item-link').attrib['href'],  
                 }
             except:
                 yield {
                     'name' :  products.css('a.product-item-link::text').get(),
-                    'price' : 'sold out',
+                    'price in £' : 'sold out',
                     'link' : products.css('a.product-item-link').attrib['href'],  
                 }
+
+        next_page = response.css('a.action.next').attrib['href']
+
+        if next_page is not None:
+            yield response.follow(next_page, callback=self.parse)
